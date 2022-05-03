49da781cb383548d585dfc5df13e56982008786e
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2022-05-03 11:55:45 -0500

    Create place-detail.module.ts

---
+++ src/app/places/place-detail/place-detail.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { PlaceDetailPageRoutingModule } from './place-detail-routing.module';
+
+import { PlaceDetailPage } from './place-detail.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    PlaceDetailPageRoutingModule
+  ],
+  declarations: [PlaceDetailPage]
+})
+export class PlaceDetailPageModule {}
